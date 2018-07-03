61ae3bfdacda2075b47dc7d5cae5da4079480557
mrsilver123 <xxsilverxd.2014@gmail.com>
2018-07-03 02:40:16 -0300

    listos todos los servicios

--- Angular/src/app/services/reserva.service.ts
+++ Angular/src/app/services/reserva.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Reserva } from '../models/reserva';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservaService {
 
-  constructor() {}
+  port='';
+
+  constructor(private _http: Http) {
+  }
+
+  getAll() {
+    return this._http.get('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/reserva/')
+      .map(res => res.json());
+  }
+
+  save(data: any) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let body = JSON.stringify(data);
+    console.log('entro service create');
+    return this._http.post('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/reserva/new', body, options)
+      .map((res: Response) => res.json());
+  }
+
+  update(data: any) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let body = JSON.stringify(data);
+    // envio en el body el mensaje transformado en un JSON
+    return this._http.post('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/reserva/' + data.id + '/edit', body, options)
+      .map((res: Response) => res.json());
+  }
+
+  delete(data: any) {
+    // utilizo el metodo delete de http que es el configurado en el deleteAction de Symfony
+    return this._http.delete(('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/reserva/' + data.id))
+      .map((res: Response) => res.json());
+  }
 }
--- Angular/src/app/services/usuario.service.ts
+++ Angular/src/app/services/usuario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Usuario } from '../models/usuario';
@@ -12,12 +12,43 @@ export class UsuarioService {
 
   userLoggedIn: boolean = false;
   userLogged: Usuario;
+  port = '';
 
-  constructor() { }
+  constructor(private _http: Http) {
+  }
+
+  getAll() {
+    return this._http.get('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/usuario/')
+      .map(res => res.json());
+  }
+
+  save(data: any) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let body = JSON.stringify(data);
+    console.log('entro service create');
+    return this._http.post('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/usuario/new', body, options)
+      .map((res: Response) => res.json());
+  }
+
+  update(data: any) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let body = JSON.stringify(data);
+    // envio en el body el mensaje transformado en un JSON
+    return this._http.post('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/usuario/' + data.id + '/edit', body, options)
+      .map((res: Response) => res.json());
+  }
+
+  delete(data: any) {
+    // utilizo el metodo delete de http que es el configurado en el deleteAction de Symfony
+    return this._http.delete(('http://localhost' + this.port + '/FinalWeb/finalProyect/web/app_dev.php/usuario/' + data.id))
+      .map((res: Response) => res.json());
+  }
 
   login(usuario: string, password: string) {
     // nico :3
-    return this.http.post('http://localhost/FinalWeb/finalProyect/web/app_dev.php/usuario/authenticate',
+    return this._http.post('http://localhost/FinalWeb/finalProyect/web/app_dev.php/usuario/authenticate',
       JSON.stringify({ usuario: usuario, password: password }))
       .map(res => res.json());
   }
