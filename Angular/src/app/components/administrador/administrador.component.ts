5648d58467780e5408e4f3c8dabe5ab30a5f8b99
mrsilver123 <xxsilverxd.2014@gmail.com>
2018-07-03 20:24:43 -0300

    Metodo de los componentes de administrador listo

--- Angular/src/app/components/administrador/administrador.component.ts
+++ Angular/src/app/components/administrador/administrador.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../models/usuario';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-administrador',
@@ -8,9 +9,71 @@ import { Usuario } from '../../models/usuario';
 })
 export class AdministradorComponent implements OnInit {
 user: Usuario = new Usuario();
-  constructor() { }
+users = [];
+btnactualizar = false;
+submitted = false;
+constructor(private servicio: UsuarioService) {
+  this.btnactualizar = false;
+}
 
-  ngOnInit() {
-  }
+onSubmit() {
+  this.submitted = true;
+}
+ngOnInit() {
+  this.refreshListUser();
+}
+
+public refreshListUser() {
+  this.servicio.getAll().subscribe(
+    result => {
+      this.users = JSON.parse(result.users);
+    },
+    error => {
+      console.log(error);
+    }
+  );
+}
+
+public updateUser(user: any) {
+  this.servicio.update(user).subscribe(
+    result => {
+      console.log('update correcto');
+      this.refreshListUser();
+    },
+    error => console.log('error: ' + error)
+  );
+}
+
+public saveUser() {
+  this.servicio.save(this.user).subscribe(
+    data => {
+      console.log('envio ok');
+      console.log('agregado correctamente.');
+      this.user = new Usuario();
+      this.refreshListUser();
+      return true;
+    },
+    error => {
+      console.error('Error saving!');
+      console.log(error);
+      return false;
+    }
+  );
+}
+
+elegirUser(user: any) {
+  this.user = this.users.filter(x => x === user).pop();
+  this.btnactualizar = true;
+}
+
+eliminarUser(user: any) {
+  this.servicio.delete(user).subscribe(
+    result => {
+      console.log('envio delete');
+      this.refreshListUser();
+    },
+    error => console.log('error: ' + error)
+  );
+}
 
 }
